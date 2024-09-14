bd77f774f2fe92aae11a69e828c88b2e3b8f17fc
KITTz1n <wellcomepudim@gmail.com>
2024-09-14 13:43:52 -0300

    Att

---
+++ Csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Csharp.Fundamentos;
+
+namespace Csharp {
+    class Program {
+        static void Main(string[] args) {
+            var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
+                {"Comentários - Fundamentos", Comentarios.Executar},
+                {"Variaveis e Constantes - Fundamentos", VariaveisEConstantes.Executar},
+            });
+
+            central.SelecionarEExecutar();
+        }
+    }
+}
